7a07d00c252f4020324f1b4b69b6ee25aebb9d2d
Rusetska Tetiana <rusetskatata@gmail.com>
2026-07-08 18:33:49 +0200

    frontend add register page

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/homepage/Home"
 import Login from "./pages/login/Login";
+import Register from "./pages/login/Register";
 
 export default function AppRouter() {
   return (
@@ -8,6 +9,7 @@ export default function AppRouter() {
 
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
 
     </Routes>
   );
--- frontend/src/pages/login/Register.tsx
+++ frontend/src/pages/login/Register.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 
-export default function Login() {
+export default function Register() {
 
     const navigate = useNavigate();
 
@@ -8,42 +8,45 @@ export default function Login() {
         <section className="min-h-screen w-screen bg-(--bgColor) text-(--mainColor) flex items-center justify-center px-6">
 
             <div className="w-full max-w-md rounded-4xl backdrop-blur-md bg-(--mainColor)/5 border border-(--mainColor)/20 shadow-2xl p-10">
-
                 <h1 className="font-['BebasNeue'] text-6xl text-center mb-12">
-                    Login
+                    LOGIN
                 </h1>
 
                 <form className="flex flex-col gap-6">
-
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        className="h-16 px-6 rounded-2xl bg-black/20 border border-(--mainColor)/20 text-xl font-['BebasNeue'] placeholder:text-gray-400 outline-none focus:border-(--mainColor)/60"
+                    />
                     <input
                         type="email"
                         placeholder="E-Mail"
                         className="h-16 px-6 rounded-2xl bg-black/20 border border-(--mainColor)/20 text-xl font-['BebasNeue'] placeholder:text-gray-400 outline-none focus:border-(--mainColor)/60"
                     />
-
                     <input
                         type="password"
                         placeholder="Passwort"
                         className="h-16 px-6 rounded-2xl bg-black/20 border border-(--mainColor)/20 text-xl font-['BebasNeue'] placeholder:text-gray-400 outline-none focus:border-(--mainColor)/60"
                     />
-
+                    <input
+                        type="password"
+                        placeholder="Passwort bestätigen"
+                        className="h-16 px-6 rounded-2xl bg-black/20 border border-(--mainColor)/20 text-xl font-['BebasNeue'] placeholder:text-gray-400 outline-none focus:border-(--mainColor)/60"
+                    />
                     <button
                         type="submit"
                         className="mt-8 font-['Kosmos'] text-4xl h-18 rounded-4xl flex items-center justify-center text-(--mainColor) backdrop-blur-md bg-(--mainColor)/5 border border-(--mainColor)/20 shadow-lg transition-all duration-300 hover:bg-(--mainColor)/10 hover:scale-[1.03] active:scale-[0.98]"
                     >
-                        Einloggen
+                        Registrieren
                     </button>
-
                 </form>
 
                 <div className="mt-10 text-center text-gray-400 font-['BebasNeue'] text-lg">
-                    Kein Konto?
+                    Bereits ein Konto?
                     <br />
-
-                    <button onClick={() => navigate("/register")} className="mt-2 text-(--mainColor) underline font-['BebasNeue']">
-                        Registrieren
+                    <button onClick={() => navigate("/login")} className="mt-2 text-(--mainColor) underline font-['BebasNeue']">
+                        Einloggen
                     </button>
-
                 </div>
 
             </div>
